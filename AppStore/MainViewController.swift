b0d6556b011934db68c2ccc66caeec1af692d16e
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-10-29 21:51:45 +0900

    update_Setting

---
+++ AppStore/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  AppStore
+//
+//  Created by 양성혜 on 2022/10/29.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+    }
+}
