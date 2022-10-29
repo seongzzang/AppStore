c4ecb93e515f7112967e9667c26cb78795c64456
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-10-29 22:39:35 +0900

    update_todayviewCOntroller

---
+++ AppStore/Scenes/Today/TodayViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TodayViewController.swift
+//  AppStore
+//
+//  Created by 양성혜 on 2022/10/29.
+//
+
+import UIKit
+import SnapKit
+
+final class TodayViewController: UIViewController {
+    
+    private lazy var colletionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "todayCell")
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(colletionView)
+        colletionView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+}
+
+extension TodayViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "todayCell", for: indexPath)
+        cell.backgroundColor = .black
+        return cell
+    }
+    
+    
+}
+
+extension TodayViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width - 32.0
+        return CGSize(width: width, height: width)
+    }
+}
