62474936c77b96d9f644b60eff498fc516c2cf9c
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-11-03 22:05:02 +0900

    update_appDetailViewController

---
+++ AppStore/Scenes/AppDetailViewController.swift
@@ -0,0 +1,109 @@
+//
+//  AppDetailViewController.swift
+//  AppStore
+//
+//  Created by 양성혜 on 2022/11/03.
+//
+
+import UIKit
+import SnapKit
+
+final class AppDetailViewController: UIViewController {
+    
+    private let appIconImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 8.0
+        
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 20.0, weight: .bold)
+        label.textColor = .label
+        
+        return label
+    }()
+    
+    private let subTitleLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .medium)
+        label.textColor = .secondaryLabel
+        
+        return label
+    }()
+    
+    private let downloadButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("받기", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13.0, weight: .bold)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12.0
+        
+        return button
+    }()
+    
+    private let shareButton: UIButton = {
+       let button = UIButton()
+        button.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        button.tintColor = .systemBlue
+        
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupViews()
+        
+        appIconImageView.backgroundColor = .lightGray
+        titleLabel.text = "title"
+        subTitleLabel.text = "subtitle"
+        
+    }
+}
+
+//MARK: Private
+private extension AppDetailViewController {
+    func setupViews(){
+        [appIconImageView,titleLabel,subTitleLabel,downloadButton,shareButton].forEach {
+            view.addSubview($0)
+        }
+        
+        appIconImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16.0)
+            $0.top.equalToSuperview().inset(32.0)
+            $0.height.equalTo(100.0)
+            $0.width.equalTo(appIconImageView.snp.height)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(appIconImageView.snp.top)
+            $0.leading.equalTo(appIconImageView.snp.trailing).offset(16.0)
+            $0.trailing.equalToSuperview().inset(16.0)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4.0)
+            $0.leading.equalTo(titleLabel.snp.leading)
+        }
+        
+        downloadButton.snp.makeConstraints {
+            $0.bottom.equalTo(appIconImageView.snp.bottom)
+            $0.height.equalTo(24.0)
+            $0.width.equalTo(60.0)
+            $0.leading.equalTo(titleLabel.snp.leading)
+        }
+        
+        shareButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(16.0)
+            $0.bottom.equalTo(appIconImageView.snp.bottom)
+            $0.height.equalTo(downloadButton.snp.height)
+            $0.width.equalTo(downloadButton.snp.height)
+        }
+        
+    }
+}
