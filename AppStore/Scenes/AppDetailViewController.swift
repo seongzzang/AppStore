25c0ccb6a385f297d0467ab96622d92b7e8ea3e8
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-11-06 23:24:32 +0900

    update_activityViewController

--- AppStore/Scenes/AppDetailViewController.swift
+++ AppStore/Scenes/AppDetailViewController.swift
@@ -53,6 +53,7 @@ final class AppDetailViewController: UIViewController {
        let button = UIButton()
         button.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
         button.tintColor = .systemBlue
+        button.addTarget(self, action: #selector(didTapShareButton), for: .touchUpInside)
         
         return button
     }()
@@ -115,6 +116,11 @@ private extension AppDetailViewController {
             $0.height.equalTo(downloadButton.snp.height)
             $0.width.equalTo(downloadButton.snp.height)
         }
-        
+    }
+    
+    @objc func didTapShareButton(){
+        let activityItems: [Any] = [today.title]
+        let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        present(activityViewController,animated: true)
     }
 }
