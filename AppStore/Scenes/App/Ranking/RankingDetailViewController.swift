46c7d1628ddbd58b4bdb9e4e5696234cf9f26fc9
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-11-26 23:41:29 +0900

    change_rankingDetailView

--- AppStore/Scenes/App/Ranking/RankingDetailViewController.swift
+++ AppStore/Scenes/App/Ranking/RankingDetailViewController.swift
@@ -10,43 +10,33 @@ import SnapKit
 
 class RankingDetailViewController : UIViewController {
     
-    private lazy var 
-    
-    private lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.delegate  = self
-        collectionView.dataSource = self
-        collectionView.isPagingEnabled = true
-        collectionView.backgroundColor = .systemBackground
-        collectionView.showsHorizontalScrollIndicator = false
-        
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "RankingDetailViewCell")
-        
-        return collectionView
-    }()
-    
+    let typeButtonView = UIView()
+    let rankingDetailView = RankingDetailView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         
-        view.backgroundColor = .red
+        setup()
     }
 }
 
-extension RankingDetailViewController: UICollectionViewDelegateFlowLayout {
-    
-}
-
-extension RankingDetailViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = UICollectionViewCell()
+extension RankingDetailViewController {
+    func setup() {
         
-        return cell
+        [typeButtonView,rankingDetailView].forEach {
+            view.addSubview($0)
+        }
+        
+        typeButtonView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(8.0)
+            $0.centerX.equalToSuperview()
+        }
+        
+        rankingDetailView.snp.makeConstraints {
+            $0.top.equalTo(typeButtonView).inset(5.0)
+            $0.leading.equalToSuperview().inset(5.0)
+        }
     }
+    
 }
